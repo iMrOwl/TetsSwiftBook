a8d0e6db1b059b521a8bb3560b8a943783a61a77
Evg.Sychev <Evg.Sychev@x5.ru>
2022-07-29 23:57:52 +0300

    Initial Commit

---
+++ TestSwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSwiftBook
+//
+//  Created by Evg.Sychev on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
